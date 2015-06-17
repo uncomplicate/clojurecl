4781ac41da8be3579a26e97f23c9f00e6336dff7
Dragan Djuric <dragandj@gmail.com>
2015-06-17 16:23:53 +0200

    Bumped the version to 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uncomplicate/clojurecl "0.1.1"
+(defproject uncomplicate/clojurecl "0.1.2-SNAPSHOT"
   :description "ClojureCL is a Clojure library for parallel computations with OpenCL."
   :url "https://github.com/uncomplicate/clojurecl"
   :scm {:name "git"
