577d33bad6de270263a31cd1462235cd33f71b0a
Dragan Djuric <dragandj@gmail.com>
2016-10-05 16:25:33 +0200

    Added license notice to source files.

--- project.clj
+++ project.clj
@@ -1,3 +1,11 @@
+;;   Copyright (c) Dragan Djuric. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) or later
+;;   which can be found in the file LICENSE at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+
 (defproject uncomplicate/clojurecl "0.6.5-SNAPSHOT"
   :description "ClojureCL is a Clojure library for parallel computations with OpenCL."
   :url "https://github.com/uncomplicate/clojurecl"
@@ -7,7 +15,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.jocl/jocl "2.0.0"]
-                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.async "0.2.385"]
                  [uncomplicate/commons "0.2.0"]
                  [vertigo "0.1.4"]]
 
@@ -16,14 +24,13 @@
           :src-linenum-anchor-prefix "L"
           :output-path "docs/codox"}
 
-  :profiles {:dev {:plugins [[lein-midje "3.1.3"]
-                             [lein-codox "0.9.4"]]
+  :profiles {:dev {:plugins [[lein-midje "3.2"]
+                             [lein-codox "0.9.5"]]
                    :global-vars {*warn-on-reflection* true
                                  *assert* true
                                  *unchecked-math* :warn-on-boxed
                                  *print-length* 128}
-                   :dependencies [[midje "1.8.3"]
-                                  [criterium "0.4.4"]]}}
+                   :dependencies [[midje "1.8.3"]]}}
 
   :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :source-paths ["src/clojure" "src/opencl"]
