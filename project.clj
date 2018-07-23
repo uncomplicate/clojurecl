875695868e4ce44740fc23e59feb284b928e131a
Dragan Djuric <dragandj@gmail.com>
2018-07-24 00:11:09 +0200

    Release 0.10.0.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/clojurecl "0.10.0-SNAPSHOT"
+(defproject uncomplicate/clojurecl "0.10.0"
   :description "ClojureCL is a Clojure library for parallel computations with OpenCL."
   :url "https://github.com/uncomplicate/clojurecl"
   :scm {:name "git"
@@ -16,8 +16,8 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.jocl/jocl "2.0.1"]
                  [org.clojure/core.async "0.4.474"]
-                 [uncomplicate/commons "0.6.0-SNAPSHOT"]
-                 [uncomplicate/fluokitten "0.9.0-SNAPSHOT"]]
+                 [uncomplicate/commons "0.6.0"]
+                 [uncomplicate/fluokitten "0.9.0"]]
 
   :codox {:metadata {:doc/formt a:markdown}
           :src-dir-uri "http://github.com/uncomplicate/clojurecl/blob/master/"
