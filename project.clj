af7e17d31aef1329a049269832bc96114120aabf
Dragan Djuric <dragandj@gmail.com>
2020-02-05 16:59:05 +0100

    Update to Neanderthal 0.27.0.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/clojurecl "0.14.0"
+(defproject uncomplicate/clojurecl "0.15.0-SNAPSHOT"
   :description "ClojureCL is a Clojure library for parallel computations with OpenCL."
   :url "https://github.com/uncomplicate/clojurecl"
   :scm {:name "git"
@@ -16,7 +16,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.jocl/jocl "2.0.2"]
                  [org.clojure/core.async "0.7.559"]
-                 [uncomplicate/commons "0.9.0"]
+                 [uncomplicate/commons "0.10.0-SNAPSHOT"]
                  [uncomplicate/fluokitten "0.9.1"]]
 
   :codox {:metadata {:doc/formt a:markdown}
