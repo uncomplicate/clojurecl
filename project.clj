3d794e8b9afa326a0c35a522e0f27d8097c49f78
Dragan Djuric <dragandj@gmail.com>
2025-07-17 13:51:31 +0200

    Release 0.16.1.

--- project.clj
+++ project.clj
@@ -6,17 +6,17 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/clojurecl "0.16.1-SNAPSHOT"
+(defproject uncomplicate/clojurecl "0.16.1"
   :description "ClojureCL is a Clojure library for parallel computations with OpenCL."
   :url "https://github.com/uncomplicate/clojurecl"
   :scm {:name "git"
         :url "https://github.com/uncomplicate/clojurecl"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.3"]
+  :dependencies [[org.clojure/clojure "1.12.1"]
                  [org.jocl/jocl "2.0.5"]
-                 [org.clojure/core.async "1.6.741"]
-                 [uncomplicate/commons "0.15.0"]
+                 [org.clojure/core.async "1.8.741"]
+                 [uncomplicate/commons "0.17.1"]
                  [uncomplicate/fluokitten "0.10.0"]]
 
   :codox {:metadata {:doc/formt a:markdown}
@@ -38,6 +38,7 @@
                                  *print-length* 128}
                    :dependencies [[midje "1.10.10"]]
                    :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"
+                                        "--enable-native-access=ALL-UNNAMED"
                                         "--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED"
                                         "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED"]}}
 
